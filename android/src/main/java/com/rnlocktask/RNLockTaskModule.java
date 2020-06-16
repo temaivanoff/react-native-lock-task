31d730a678cc69804e99fce596d38a38583e8b3b
Tobias Duehr <tobias@dieschittigs.de>
2020-06-16 13:57:15 +0200

    added promises

--- android/src/main/java/com/rnlocktask/RNLockTaskModule.java
+++ android/src/main/java/com/rnlocktask/RNLockTaskModule.java
@@ -6,11 +6,19 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 public class RNLockTaskModule extends ReactContextBaseJavaModule {
+
+
+  private static final String ACTIVITY_GONE = "ACTIVITY_GONE";
+  private static final String DEVICE_OWNER_CLEARED = "DEVICE_OWNER_CLEARED";
+  private static final String LOCKED_TASK = "LOCKED_TASK";
+  private static final String LOCKED_TASK_AS_OWNER = "LOCKED_TASK_AS_OWNER";
+  private static final String UNLOCKED_TASK = "UNLOCKED_TASK";
 
   public RNLockTaskModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -22,19 +30,22 @@ public class RNLockTaskModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public  void clearDeviceOwnerApp() {
+  public  void clearDeviceOwnerApp(Promise promise) {
     try {
       Activity mActivity = getCurrentActivity();
       if (mActivity != null) {
         DevicePolicyManager myDevicePolicyManager = (DevicePolicyManager) mActivity.getSystemService(Context.DEVICE_POLICY_SERVICE);
         myDevicePolicyManager.clearDeviceOwnerApp(mActivity.getPackageName());
+        promise.resolve(DEVICE_OWNER_CLEARED);
       }
+      promise.reject(ACTIVITY_GONE, "Activity gone or mismatch");
     } catch (Exception e) {
+      promise.reject(e);
     }
   }
 
   @ReactMethod
-  public void startLockTask() {
+  public void startLockTask(Promise promise) {
     try {
       Activity mActivity = getCurrentActivity();
       if (mActivity != null) {
@@ -45,22 +56,31 @@ public class RNLockTaskModule extends ReactContextBaseJavaModule {
           String[] packages = {mActivity.getPackageName()};
           myDevicePolicyManager.setLockTaskPackages(mDPM, packages);
           mActivity.startLockTask();
+          promise.resolve(LOCKED_TASK_AS_OWNER);
         } else {
           mActivity.startLockTask();
+          promise.resolve(LOCKED_TASK);
         }
+      } else{
+        promise.reject(ACTIVITY_GONE, "Activity gone or mismatch");
       }
     } catch (Exception e) {
+      promise.reject(e);
     }
   }
 
   @ReactMethod
-  public  void stopLockTask() {
+  public  void stopLockTask(Promise promise) {
     try {
       Activity mActivity = getCurrentActivity();
       if (mActivity != null) {
         mActivity.stopLockTask();
+        promise.resolve(UNLOCKED_TASK);
+      } else {
+          promise.reject(ACTIVITY_GONE, "Activity gone or mismatch");
       }
     } catch (Exception e) {
+      promise.reject(e);
     }
   }
 }
